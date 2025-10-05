5b0785bb86dcff38bb6d9226e2b9084811da37b4
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-05 07:14:43 -0400

    test route check

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ import uvicorn
 
 app = FastAPI(title='AI Receptionist POC')
 
+@app.get("/")
+async def root():
+    return {"message": "Welcome to the AI Assistant API"}
 #Register Routes
 app.include_router(calls.router, prefix="/calls",tags=["Calls"])
 app.include_router(booking.router, prefix="/bookings",tags=["Booking"])
 
 if __name__=="__main__":
-    @app.get("/")
-    async def root():
-        return {"message": "Welcome to the AI Assistant API"}
+    
     uvicorn.run("app.main:app", host="0.0.0.0", port=8000,reload=True)
