a016b2faab241bc234f6f32343607d74e9a7838d
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-05 07:22:32 -0400

    added temporary endpoint to trigger initialization

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import calls, booking
+from app.models import Base
+from app.database import engine
 import uvicorn
 
 app = FastAPI(title='AI Receptionist POC')
@@ -7,6 +9,12 @@ app = FastAPI(title='AI Receptionist POC')
 @app.get("/")
 async def root():
     return {"message": "Welcome to the AI Assistant API"}
+
+@app.get("/init-db")
+async def init_db():
+    Base.metadata.create_all(bind=engine)
+    return {"message": "Database initialized"}
+
 #Register Routes
 app.include_router(calls.router, prefix="/calls",tags=["Calls"])
 app.include_router(booking.router, prefix="/bookings",tags=["Booking"])
