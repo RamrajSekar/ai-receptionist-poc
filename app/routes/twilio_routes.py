509ffc507ecd665d2e8a08edfa85ad778b4121ff
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-06 23:45:55 -0400

    added new line to say end the call

--- app/routes/twilio_routes.py
+++ app/routes/twilio_routes.py
@@ -17,9 +17,10 @@ async def voice_handler(From: str = Form('4757770732'),To: str = Form('662547379
     try:
         logger.info(f"Incoming Call from {From} to {To}")
         resp = VoiceResponse()
-        resp.say("Hello! You've reached the AI Receptionist. Please say your name followed by date and time for your appointment after the beep and press # key to end!")
-        resp.gather(finish_on_key='#')
+        resp.say("Hello! You've reached the AI Receptionist. Please say your name followed by date and time for your appointment after the beep!")
         resp.record(max_length=20, play_beep=True, action="/process_recording")
+        resp.say("Press the # key to end the call!")
+        resp.gather(finish_on_key='#')
         return Response(content=str(resp),media_type="application/xml")
     except Exception as e:
         logger.error(f"Error Occurred: {str(e)}")
