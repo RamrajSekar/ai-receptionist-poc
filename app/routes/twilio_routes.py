9182b92586f6ea2719da86f428b9e91e67cda8b5
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-06 23:51:13 -0400

    added phone number to say

--- app/routes/twilio_routes.py
+++ app/routes/twilio_routes.py
@@ -17,7 +17,7 @@ async def voice_handler(From: str = Form('4757770732'),To: str = Form('662547379
     try:
         logger.info(f"Incoming Call from {From} to {To}")
         resp = VoiceResponse()
-        resp.say("Hello! You've reached the AI Receptionist. Please say your name followed by date and time for your appointment after the beep!")
+        resp.say("Hello! You've reached the AI Receptionist. Please say your name and phone number followed by date and time for your appointment after the beep!")
         resp.record(max_length=20, play_beep=True, action="/process_recording")
         resp.say("Press the # key to end the call!")
         resp.gather(finish_on_key='#')
