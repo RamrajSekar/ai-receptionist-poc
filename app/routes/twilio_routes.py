f773df132437503997c499cf7aa4268c959ae12a
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-06 23:06:59 -0400

    added step to download recording and covert audio to text

--- app/routes/twilio_routes.py
+++ app/routes/twilio_routes.py
@@ -2,15 +2,20 @@ from fastapi import APIRouter, Form
 from fastapi.responses import Response
 from twilio.twiml.voice_response import VoiceResponse
 import logging
+import requests
+import openai
+import os
 
 router = APIRouter()
 logger = logging.getLogger(__name__)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @router.post("/voice")
 async def voice_handler(From: str = Form('4757770732'),To: str = Form('6625473791'),):
     logger.info(f"Incoming Call from {From} to {To}")
     resp = VoiceResponse()
-    resp.say("Hello! You've reached the Receoptionist. Please say your name and date for your appointment after the beep!")
+    resp.say("Hello! You've reached the AI Receptionist. Please say your name followed by date and time for your appointment after the beep!")
     resp.record(max_length=20, play_beep=True, action="/process_recording")
 
     return Response(content=str(resp),media_type="application/xml")
@@ -19,7 +24,17 @@ async def voice_handler(From: str = Form('4757770732'),To: str = Form('662547379
 async def process_recording(RecordingUrl: str = Form(...),From: str = Form(...)):
     logger.info(f"Recording from {From}: {RecordingUrl}")
 
-    # ToDo: Download and do speech to text later
+    # Step 1: Download audio as wav same as Twilio format
+    audio_file = "call_recording.wav"
+    resp = requests.get(f"{RecordingUrl}.wav")
+    with open(audio_file, "wb") as f:
+        f.write(resp.content)
+    #Step 2: Transcribe to Whisper
+    with open(audio_file, "rb") as f:
+        transcribe = openai.audio.transcriptions.create(model="whisper-1",file=f)
+    user_text = transcribe.text
+    logger.info(f"Transcribed text from {From}: {user_text}")
+    #Step 3: Respond back to user
     resp = VoiceResponse()
     resp.say("Thank you, We received you apoointment request!")
     return Response(content=str(resp),media_type="application/xml")
