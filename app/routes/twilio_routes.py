386793feff1f1454dc5c9f8d70efa3564f373149
Ramraj Sekar <sekar.ramraj@gmail.com>
2025-10-07 21:43:41 -0400

    skipping gather

--- app/routes/twilio_routes.py
+++ app/routes/twilio_routes.py
@@ -19,8 +19,8 @@ async def voice_handler(From: str = Form('4757770732'),To: str = Form('662547379
         resp = VoiceResponse()
         resp.say("Hello! You've reached the AI Receptionist. Please say your name and phone number followed by date and time for your appointment after the beep!")
         resp.record(max_length=20, play_beep=True, action="/process_recording")
-        resp.say("Press the # key to end the call!")
-        resp.gather(finish_on_key='#')
+        # resp.say("Press the # key to end the call!")
+        # resp.gather(finish_on_key='#')
         return Response(content=str(resp),media_type="application/xml")
     except Exception as e:
         logger.error(f"Error Occurred: {str(e)}")
